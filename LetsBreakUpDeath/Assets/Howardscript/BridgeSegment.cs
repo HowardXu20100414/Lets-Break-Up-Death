0cf143cfce51e4b357bf2d6ea743be160e6b3efa
riolg <rio.lee.garcia@gmail.com>
2025-07-14 17:58:56 -0400

    idek 🤷
    some changes ig

--- LetsBreakUpDeath/Assets/Howardscript/BridgeSegment.cs
+++ LetsBreakUpDeath/Assets/Howardscript/BridgeSegment.cs
@@ -1,13 +1,14 @@
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
 
-public class BridgeFallingPiece : MonoBehaviour
+public class BridgeSegment : MonoBehaviour
 {
     public float delayBetweenPieces;
     [HideInInspector]
     public int fallIndex = 0;
     public float destroyAfter = 5f;
-    public bool firstBridgePiece = false;
+
+    public bool isFirst = false;
     Rigidbody2D rb;
 
     void Awake()
@@ -18,10 +19,11 @@ public class BridgeFallingPiece : MonoBehaviour
 
     void Start()
     {
-        if (!firstBridgePiece)
+        if (!isFirst)
         {
             delayBetweenPieces = Random.Range(1.5f, 2.5f);
-        } else
+        }
+        else
         {
             delayBetweenPieces = 5f;
         }
@@ -34,18 +36,19 @@ public class BridgeFallingPiece : MonoBehaviour
 
     void AssignFallIndex()
     {
+        // Find all BridgeFallingPiece in the scene (or limit to parent children)
         // Find all BridgeFallingPiece in the scene 
-        BridgeFallingPiece[] allPieces = null;
+        BridgeSegment[] allPieces = null;
 
         if (transform.parent != null)
         {
             // If the pieces are siblings under the same parent
-            allPieces = transform.parent.GetComponentsInChildren<BridgeFallingPiece>();
+            allPieces = transform.parent.GetComponentsInChildren<BridgeSegment>();
         }
         else
         {
             // Otherwise find all in scene (less efficient)
-            allPieces = FindObjectsOfType<BridgeFallingPiece>();
+            allPieces = FindObjectsOfType<BridgeSegment>();
         }
 
         // Sort by x position ascending (left to right)
--- LetsBreakUpDeath/Assets/Scripts/BridgeSpawner.cs
+++ LetsBreakUpDeath/Assets/Scripts/BridgeSpawner.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BridgeSpawner : MonoBehaviour
 {
-    public float bridgeSegmentLength = 7f;
     public int bridgeSegments = 10;
     public GameObject bridgePrefab;
     void Start()
     {
+        BridgeSegment bridgeSegmentScript = bridgePrefab.GetComponent<BridgeSegment>();
+
+        float bridgePrefabLength = bridgePrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+
         transform.position = new Vector2(0, 0);
 
         for (int i = 0; i < bridgeSegments; i++)
         {
-            Instantiate(bridgePrefab, new Vector3(bridgeSegmentLength * i, transform.position.y, transform.position.z), Quaternion.identity);
+            Instantiate(bridgePrefab, new Vector3(bridgePrefabLength * i, transform.position.y, transform.position.z), Quaternion.identity);
+            if (i == 0)
+            {
+                bridgeSegmentScript.isFirst = true;
+            }
         }
 
     }
